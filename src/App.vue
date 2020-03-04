f8c662502cd10cded7e81a620ab7a06ca35404be
mismaah-abdulla <mismaahabdulla@gmail.com>
2020-03-04 22:29:19 +0800

    :)

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <v-app>
     <Drawer></Drawer>
+    <v-content>
+      <v-container fluid>
 
-    
+      </v-container>
+    </v-content>
     <Footer></Footer>
   </v-app>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld';
 import Footer from './components/Footer'
 import Drawer from './components/Drawer'
 
 export default {
-  name: 'Stock Portfolio',
+  name: 'App',
   components: {
     Footer,
     Drawer
@@ -21,6 +23,5 @@ export default {
   // data: () => ({
   //   drawer: null,
   // }),
-  
-};
+}
 </script>
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -3,6 +3,7 @@
     <v-navigation-drawer
       v-model="drawer"
       app
+      expand-on-hover
     >
       <v-list dense>
         <v-list-item link>
@@ -10,7 +11,7 @@
             <v-icon>mdi-home</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
+            <v-list-item-title>Watchlist</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item link>
@@ -18,7 +19,15 @@
             <v-icon>mdi-contact-mail</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
+            <v-list-item-title>Portfolio</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>News Feed</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -30,7 +39,7 @@
         dense
       >
         <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-        <v-toolbar-title>Portfolio</v-toolbar-title>
+        <v-toolbar-title>Stocks</v-toolbar-title>
     </v-app-bar>
   </div>
 </template>

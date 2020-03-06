c57cb515592be93b622f4b8861b227c7065dc03b
Mismaah Abdulla <mismaahabdulla@gmail.com>
2020-03-06 18:17:50 +0800

    Separate symbol and code from fetch url

--- src/components/GChart.vue
+++ src/components/GChart.vue
@@ -1,9 +1,11 @@
 <template>
-    <GChart v-if="loaded"
-    type="CandlestickChart"
-    :data="stockData"
-    :options="options"
-    />
+    <v-container fluid>
+      <GChart v-if="loaded"
+      type="CandlestickChart"
+      :data="stockData"
+      :options="options"
+      />
+    </v-container>
 </template>
 <script>
 import { GChart } from 'vue-google-charts'
@@ -27,21 +29,29 @@ export default {
   },
   mounted () {
     this.loaded = false
-    let eodAPI = 'http://localhost/AAPLeod'
+    let exchangeCode = 'US'
+    let symbol = 'AAPL'
+    let eodAPI = `http://localhost/API/EOD/${exchangeCode}/${symbol}`
     try{
       fetch(eodAPI)
       .then(response => response.json())
       .then(data =>{
         console.log(data)
-        for(let i = data.length-30; i < data.length; i++){
+        for(let i = data.length-60; i < data.length; i++){
           let moment = require('moment')
           let date = new Date(data[i].date)
           let daymonth = moment(new Date(date)).format("DD MMM")
           this.stockData.push([daymonth, data[i].low, data[i].open, data[i].close, data[i].high])
         }
-        this.options = {  
-          height: '600',
-          legend: 'none',
+        this.options = { 
+          legend: 'none', 
+          chartArea: {
+            top: 50,
+            left: 50,
+            right: 50,
+          },
+          height: '500',
+          // width: '2000',
           candlestick: {
             fallingColor: { strokeWidth: 0, fill: '#a52714' },
             risingColor: { strokeWidth: 0, fill: '#0f9d58' }

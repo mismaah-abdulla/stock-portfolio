afa2951b9fc795c151557258273fb33bbdc1bcf9
Mismaah Abdulla <mismaahabdulla@gmail.com>
2020-03-11 12:42:39 +0800

    Add loading icon for chart

--- src/components/GChart.vue
+++ src/components/GChart.vue
@@ -10,6 +10,14 @@
           <v-btn outlined @click="renderChart(fetchedData, button.duration)">{{ button.text }}</v-btn>
         </div>
       </v-row>
+      <v-row v-if="!loaded" class="pt-5" justify="center">
+        <v-progress-circular class="pt-5"
+          :size="70"
+          :width="6"
+          color="grey"
+          indeterminate
+        ></v-progress-circular>
+      </v-row>
       <GChart v-if="loaded"
       type="CandlestickChart"
       :data="stockData"

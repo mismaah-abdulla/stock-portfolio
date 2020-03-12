fc7a6606acd0bbb6065ad7e2ca3555a12e7c14f3
Mismaah Abdulla <mismaahabdulla@gmail.com>
2020-03-12 14:50:33 +0800

    Add router, move Markets into views

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,14 @@
 <template>
   <v-app>
     <Drawer></Drawer>
-    <v-content>
-      <v-container fluid>
-        <Search></Search>
-        
-      </v-container>
-    </v-content>
+    <router-view/>
     <Footer></Footer>
   </v-app>
 </template>
-
 <script>
 import Footer from './components/Footer'
 import Drawer from './components/Drawer'
-import Search from './components/Search'
-
 export default {
   name: 'App',
-  components: {
-    Footer,
-    Drawer,
-    Search,
-  },
 }
 </script>

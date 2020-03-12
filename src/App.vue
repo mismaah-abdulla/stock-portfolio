7adabb66a8630c42512d2bc9ad8ce5e1492d2b5a
Mismaah Abdulla <mismaahabdulla@gmail.com>
2020-03-12 15:33:55 +0800

    Remove footer

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,13 @@
     <v-content>
       <router-view/>
     </v-content>
-    <Footer></Footer>
   </v-app>
 </template>
 <script>
-import Footer from './components/Footer'
 import Drawer from './components/Drawer'
 export default {
   name: 'App',
   components: {
-    Footer,
     Drawer
   }
 }

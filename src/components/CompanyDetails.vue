4d1d6e797ad14dcd7b303f31d9d0f6350ffe017a
Mismaah Abdulla <mismaahabdulla@gmail.com>
2020-03-12 18:24:16 +0800

    Modify company details componenet

--- src/components/CompanyDetails.vue
+++ src/components/CompanyDetails.vue
@@ -1,5 +1,28 @@
 <template>
-  <div v-if="details">{{ details.Name }}</div>
+  <div>
+    <v-row v-if="!loaded" class="pt-5 pl-5" justify="left">
+      <v-progress-circular
+        :size="50"
+        :width="5"
+        color="grey"
+        indeterminate
+      ></v-progress-circular>
+    </v-row>
+    <div v-if="details">
+      <v-list>
+        <v-list-item>
+          <v-list-item-avatar v-if="details.LogoURL">
+            <v-img :src="logoURL"></v-img>
+          </v-list-item-avatar>
+          <v-list-item-content class="py-0">
+            <v-list-item-title class="title" v-text="details.Code"></v-list-item-title>
+            <v-list-item-subtitle v-text="details.Name"></v-list-item-subtitle>
+            <v-list-item-subtitle>Exchange: {{ details.Exchange }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,7 +30,8 @@ export default {
   data () {
     return {
       loaded: false,
-      details: null
+      details: null,
+      logoURL: null
     }
   },
   props: {
@@ -23,6 +47,7 @@ export default {
         .then(response => response.json())
         .then(data =>{
           this.details = data
+          if(this.details.LogoURL) this.logoURL =`https://eodhistoricaldata.com/${this.details.LogoURL}`
           this.loaded = true
         })
       }catch(e){

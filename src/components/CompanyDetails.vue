e35448d9f238fcc830b5d88828a1ef60d5135314
Mismaah Abdulla <mismaahabdulla@gmail.com>
2020-03-13 11:33:34 +0800

    Modify UI

--- src/components/CompanyDetails.vue
+++ src/components/CompanyDetails.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-row v-if="!loaded" class="pt-5 pl-5">
+    <v-row v-if="!loaded" class="pt-5" justify="center">
       <v-progress-circular
         :size="50"
         :width="5"
@@ -8,21 +8,36 @@
         indeterminate
       ></v-progress-circular>
     </v-row>
-    <div v-if="details">
-      <v-list>
-        <v-list-item>
-          <v-list-item-avatar v-if="details.LogoURL">
-            <v-img :src="logoURL"></v-img>
-          </v-list-item-avatar>
-          <v-list-item-content class="py-0">
-            <v-list-item-title class="title" v-text="details.Code"></v-list-item-title>
-            <v-list-item-subtitle v-text="details.Name"></v-list-item-subtitle>
-            <v-list-item-subtitle v-if="details.Exchange" >Exchange: {{ details.Exchange }}</v-list-item-subtitle>
-            <v-list-item-subtitle v-else >Exchange: n/a </v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </div>
+    <v-row v-else>
+      <v-spacer></v-spacer>
+      <v-col>
+        <v-list>
+          <v-list-item>
+            <v-list-item-avatar v-if="details.LogoURL">
+              <v-img :src="logoURL"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content class="py-0">
+              <v-list-item-title class="title" v-text="details.Code"></v-list-item-title>
+              
+              <v-list-item-subtitle v-if="details.Exchange" >Exchange: {{ details.Exchange }}</v-list-item-subtitle>
+              <v-list-item-subtitle v-else >Exchange: n/a </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-col>
+      <v-col>
+        <v-list>
+          <v-list-item>
+            <v-list-item-content class="py-0">
+              <v-list-item-title class="title" v-text="details.Name"></v-list-item-title>
+              <v-list-item-subtitle v-if="details.Sector"> Sector: {{ details.Sector }} </v-list-item-subtitle>
+              <v-list-item-subtitle v-else> Sector: n/a </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-col>
+      <v-spacer></v-spacer> 
+    </v-row>
   </div>
 </template>
 

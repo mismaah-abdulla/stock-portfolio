944b19b4e465e49de078a3862081f1854693c9c3
mismaah-abdulla <mismaahabdulla@gmail.com>
2020-04-03 12:55:50 +0800

    Add login

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <v-app>
-    <Drawer></Drawer>
-    <v-content>
+    <Login v-if="!loggedIn"/>
+    <Drawer v-if="loggedIn"></Drawer>
+    <v-content v-if="loggedIn">
       <router-view :key="$route.fullPath"/>
     </v-content>
   </v-app>
 </template>
 <script>
 import Drawer from './components/Drawer'
+import Login from './views/Login'
 export default {
   name: 'App',
   components: {
-    Drawer
-  }
+    Drawer,
+    Login
+  },
+  data: () => ({
+    loggedIn: true,
+  })
 }
 </script>

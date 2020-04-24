cc76088e67396323a9f2b68bfd2ff5082f5a2ce0
mismaah-abdulla <mismaahabdulla@gmail.com>
2020-04-24 12:42:40 +0800

    Remove login from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <v-app>
-    <Login @loggedIn="loggedIn = true" v-if="!loggedIn"/>
     <Drawer v-if="(this.$route.name != 'Register') && (this.$route.name != 'Login')" :drawer="drawer"></Drawer>
-    <v-content v-if="loggedIn">
+    <v-content>
       <router-view :key="$route.fullPath" @drawer="drawer = !drawer"/>
     </v-content>
   </v-app>
 </template>
 <script>
 import Drawer from './components/Drawer'
-import Login from './views/Login'
 export default {
   name: 'App',
   components: {
     Drawer,
-    Login
   },
   data: () => ({
-    loggedIn: false,
     drawer: null
   })
 }
